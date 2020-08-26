76ad2b616b55ca35a44be1dc6c05b96cfae05ea5
Graeme Rocher <graeme.rocher@oracle.com>
2020-08-26 11:16:20 +0200

    Shift pet service config http services, add @Recoverable definition

--- src/main/java/example/atp/services/PetHealthService.java
+++ src/main/java/example/atp/services/PetHealthService.java
@@ -1,13 +1,13 @@
 package example.atp.services;
 
-import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Recoverable;
 
 import java.util.concurrent.CompletableFuture;
 
-@Requires(property = "pet.health.endpoint")
-@Client("${pet.health.endpoint}")
+@Client(value = "pet-health")
+@Recoverable(api = PetHealthOperations.class)
 public interface PetHealthService extends PetHealthOperations {
     @Override
     @Get("/pets/{name}/health")
