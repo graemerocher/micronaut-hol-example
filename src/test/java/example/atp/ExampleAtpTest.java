f090af6ef7fac7c5d07e3a52b61748cc2110d414
graemerocher <graeme.rocher@gmail.com>
2020-09-11 13:54:24 +0200

    Define application in lab4 state

--- src/test/java/example/atp/ExampleAtpTest.java
+++ src/test/java/example/atp/ExampleAtpTest.java
@@ -10,6 +10,10 @@ import javax.inject.Inject;
 @MicronautTest
 public class ExampleAtpTest {
 
+    static {
+        System.setProperty("oracle.jdbc.fanEnabled", "false");
+    }
+
     @Inject
     EmbeddedApplication application;
 
