c085ea5f16c6754c628f76f734262c18a28d7f99
graemerocher <graeme.rocher@gmail.com>
2020-09-11 13:57:38 +0200

    Revert "Lab 5 state"
    This reverts commit 013f30ae3250aa1444fb6be05a38476e826be7bd.

--- src/main/java/example/atp/Application.java
+++ src/main/java/example/atp/Application.java
@@ -27,6 +27,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        System.setProperty("oracle.jdbc.fanEnabled", "false");
         Micronaut.run(Application.class);
     }
 
--- src/main/java/example/atp/controllers/PetController.java
+++ src/main/java/example/atp/controllers/PetController.java
@@ -2,10 +2,12 @@ package example.atp.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import example.atp.domain.NameDTO;
 import example.atp.domain.Pet;
 import example.atp.repositories.PetRepository;
+import example.atp.services.PetHealthOperations;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
@@ -13,9 +15,11 @@ import io.micronaut.http.annotation.Get;
 class PetController {
 
     private final PetRepository petRepository;
+    private final PetHealthOperations petHealthOperations;
 
-    PetController(PetRepository petRepository) {
+    PetController(PetRepository petRepository, PetHealthOperations petHealthOperations) {
         this.petRepository = petRepository;
+        this.petHealthOperations = petHealthOperations;
     }
 
     @Get("/")
@@ -26,5 +30,10 @@ class PetController {
     @Get("/{name}")
     Optional<Pet> byName(String name) {
         return petRepository.findByName(name);
+    }
+
+    @Get("/{name}/health")
+    CompletableFuture<PetHealthOperations.PetHealth> getHealth(String name) {
+        return petHealthOperations.getHealth(name);
     }
 }
