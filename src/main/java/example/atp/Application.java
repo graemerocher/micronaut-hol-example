a9bead2dc6819d6ff440929e7db78d93efa67944
graemerocher <graeme.rocher@gmail.com>
2020-09-11 13:58:03 +0200

    Lab 6 state

--- src/main/java/example/atp/Application.java
+++ src/main/java/example/atp/Application.java
@@ -27,7 +27,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        System.setProperty("oracle.jdbc.fanEnabled", "false");
         Micronaut.run(Application.class);
     }
 
