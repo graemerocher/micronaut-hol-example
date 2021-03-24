7bd194a5892c300649aaccdf92899a81183800e7
graemerocher <graeme.rocher@gmail.com>
2021-03-24 11:48:52 +0100

    Simplify example

--- src/main/java/example/atp/controllers/PetController.java
+++ src/main/java/example/atp/controllers/PetController.java
@@ -2,12 +2,9 @@ package example.atp.controllers;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-
 import example.atp.domain.NameDTO;
 import example.atp.domain.Pet;
 import example.atp.repositories.PetRepository;
-import example.atp.services.PetHealthOperations;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.scheduling.TaskExecutors;
@@ -18,11 +15,9 @@ import io.micronaut.scheduling.annotation.ExecuteOn;
 class PetController {
 
     private final PetRepository petRepository;
-    private final PetHealthOperations petHealthOperations;
 
-    PetController(PetRepository petRepository, PetHealthOperations petHealthOperations) {
+    PetController(PetRepository petRepository) {
         this.petRepository = petRepository;
-        this.petHealthOperations = petHealthOperations;
     }
 
     @Get("/")
@@ -33,10 +28,5 @@ class PetController {
     @Get("/{name}")
     Optional<Pet> byName(String name) {
         return petRepository.findByName(name);
-    }
-
-    @Get("/{name}/health")
-    CompletableFuture<PetHealthOperations.PetHealth> getHealth(String name) {
-        return petHealthOperations.getHealth(name);
     }
 }
--- src/test/java/example/atp/ExampleAtpTest.java
+++ src/test/java/example/atp/ExampleAtpTest.java
@@ -1,7 +1,7 @@
 package example.atp;
 
 import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -11,7 +11,7 @@ import javax.inject.Inject;
 public class ExampleAtpTest {
 
     @Inject
-    EmbeddedApplication application;
+    EmbeddedApplication<?> application;
 
     @Test
     void testItWorks() {
